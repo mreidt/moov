5f0c11006f8d49aa6c4e346957a05f849078996b
Matheus Eidt <matheus@mx2.unisc.br>
2021-07-21 21:35:53 -0300

    Criando usuário customizado, que utiliza email como login.

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager,
+                                        PermissionsMixin)
 
-# Create your models here.
+
+class GerenciadorUsuario(BaseUserManager):
+
+    def cria_usuario(self, email, nome, senha=None, **extra_fields):
+        """Cria e salva um novo usuário"""
+        if not email:
+            raise ValueError('Usuários devem ter um email válido.')
+        if not nome:
+            raise ValueError('Usuários devem ter um nome válido.')
+        usuario = self.model(
+            email=self.normalize_email(email),
+            nome=nome,
+            **extra_fields
+        )
+        usuario.set_password(senha)
+        usuario.save(using=self._db)
+
+        return usuario
+
+
+class Usuario(AbstractBaseUser, PermissionsMixin):
+    """Modelo de usuário customizado, com login através do email"""
+    email = models.EmailField(max_length=255, unique=True)
+    nome = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+
+    objects = GerenciadorUsuario()
+
+    USERNAME_FIELD = 'email'
